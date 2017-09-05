8bbae9e59492353241d4a3e0a30af589f35fa108
Stan Sakl <stansakl@gmail.com>
2017-09-05 17:55:26 -0500

    Remove failing assertion from test stub.

--- tests/VolunTrackTest.php
+++ tests/VolunTrackTest.php
@@ -4,6 +4,6 @@ class VoluntrackTest extends PHPUnit_Framework_TestCase
     // Individual tests go here
     public function testThatItWorks($value='')
     {
-        $this->assertEquals(1,0);
+        //$this->assertEquals(1,1);
     }
 }
