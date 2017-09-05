838c1d5214e5c83a860a465d8d81b38fcdcafb5b
Stan Sakl <stansakl@gmail.com>
2017-09-05 11:06:54 -0500

    Add phpinfo() call for now.

--- src/index.php
+++ src/index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
+<head>
 <link rel="stylesheet" type="text/css" href="/css/voluntrack.css">
 <script src="/js/voluntrack.js"></script>
+</head>
 <?php
 //namespace voluntrack;
 require "user.php";
-
 echo "It works!<br>";
 
 $user = new voluntrack\User();
 $user->name = "new user";
 
-echo "Hello $user->name!";
+echo "Hello $user->name!<br><br>";
+echo phpinfo();
+
 ?>
