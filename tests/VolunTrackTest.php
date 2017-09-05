4d2d6b7205d09a045dda817f259b5bfdd66ec996
Stan Sakl <stansakl@gmail.com>
2017-09-05 12:34:27 -0500

    Install phpunit and stub out a failing test. TODO: Replace with real tests.

---
+++ tests/VolunTrackTest.php
@@ -0,0 +1,9 @@
+<?php
+class VoluntrackTest extends PHPUnit_Framework_TestCase
+{
+    // Individual tests go here
+    public function testThatItWorks($value='')
+    {
+        $this->assertEquals(1,0);
+    }
+}
