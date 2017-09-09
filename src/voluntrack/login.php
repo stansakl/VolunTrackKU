301bcdae3cd55764d45e4302b69c1797e1765a61
Stan Sakl <stansakl@gmail.com>
2017-09-09 13:51:54 -0500

    Move vendor folder and test we can find 3rd party components.

---
+++ src/voluntrack/login.php
@@ -0,0 +1,10 @@
+<?php
+namespace voluntrack;
+require '../vendor/autoload.php';
+
+// 2. Instantiate Guzzle HTTP client
+$client = new \GuzzleHttp\Client();
+
+echo "This is the login page!";
+
+ ?>
---
+++ src/voluntrack/user.php
@@ -0,0 +1,16 @@
+<?php
+namespace voluntrack;
+
+/**
+* The User class holds details of the user.
+*/
+class User
+{
+    public $last_name = '';
+    public $first_name = '';
+    public $email = '';
+
+    function __construct() {
+
+    }
+}
