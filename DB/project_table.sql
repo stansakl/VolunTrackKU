6e4d6c474b31700c41c6131168fab7ef7f363935
Stan Sakl <stansakl@gmail.com>
2017-10-22 08:25:05 -0500

    Add additional database scripts

---
+++ DB/project_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`PROJECT` (
+  `Project_id` INT NOT NULL AUTO_INCREMENT,
+  `Project_Name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Project_id`),
+  UNIQUE INDEX `Project_id_UNIQUE` (`Project_id` ASC),
+  UNIQUE INDEX `Project_Name_UNIQUE` (`Project_Name` ASC))
+ENGINE = InnoDB
