b26dd102922812e81686c5725ce93d95300c3d7d
Stan Sakl <stansakl@gmail.com>
2017-11-12 14:58:00 -0600

    Add basic time entry form.

--- src/voluntrack/timeentry_view.php
+++ src/voluntrack/timeentry_view.php
@@ -4,17 +4,40 @@ require "htmlconstants.php";
 //namespace voluntrack;
 //require '../vendor/autoload.php';
 ?>
- <?php
- ?>
+
  <!DOCTYPE html>
  <?php echo HEADER; ?>
 
 
  <body>
-     <main class="container">
-         <form class="col-md-12" action="timeentry.php" method="post">
-             <input type="submit" name="loginbtn" value="Enter My Time">
-         </form>
-     </main>
+ <?php require "navbar.php"; ?>
+	<div class="container">
+	  <div class="panel panel-primary">
+	  <div class="panel-heading">Time Entry</div>
+	  <div class="panel-body">
+        <form  action="" method="post">
+		    <div class="form-group">
+                <label for="project">Enter the project you worked on.</label>
+				<input type="text" class="form-control input-lg" name="project"
+                value="" placeholder="Project">
+			</div>
+            <div class="form-group">
+                <label for="startdate">Enter the start date</label>
+				<input type="date" class="form-control input-lg" name="startdate"><br>
+                <label for="starttime">Enter the start time</label>
+				<input type="time" class="form-control input-lg" name="starttime">
+			</div>
+            <div class="form-group">
+                <label for="enddate">Enter the end date</label>
+				<input type="date" class="form-control input-lg" name="enddate"><br>
+                <label for="endtime">Enter the end time</label>
+				<input type="time" class="form-control input-lg" name="endtime">
+			</div>
 
+             <button type="submit" class="btn btn-primary" name="timeentrybtn">Enter Time</button>
+        </form>
+	  </div>
+	  </div>
+	  </div>
+	</div>
  </body>
