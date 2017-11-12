575082da13006794ba9148cc97f93a382b90857d
Stan Sakl <stansakl@gmail.com>
2017-11-12 16:26:31 -0600

    Add styling to timereport_view.php

--- src/voluntrack/timereport_view.php
+++ src/voluntrack/timereport_view.php
@@ -4,15 +4,20 @@ require "htmlconstants.php";
 //namespace voluntrack;
 //require '../vendor/autoload.php';
 ?>
- <?php
- ?>
+
  <!DOCTYPE html>
  <?php echo HEADER; ?>
 
 
  <body>
-     <main class="container">
-        <h2>Time Report Goes Here!</h2>
-     </main>
-
+ <?php require "navbar.php"; ?>
+	<div class="container">
+	  <div class="panel panel-primary">
+	  <div class="panel-heading">Time Report for <?php $_SESSION['user'] ?></div>
+	  <div class="panel-body">
+        
+	  </div>
+	  </div>
+	  </div>
+	</div>
  </body>
