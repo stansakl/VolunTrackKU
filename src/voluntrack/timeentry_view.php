65e39f7aeb42d5623f00dd0fa99f8f3c6ea17dec
Stan Sakl <stansakl@gmail.com>
2017-11-12 15:10:03 -0600

    Change project to dropdown

--- src/voluntrack/timeentry_view.php
+++ src/voluntrack/timeentry_view.php
@@ -16,21 +16,27 @@ require "htmlconstants.php";
 	  <div class="panel-heading">Time Entry</div>
 	  <div class="panel-body">
         <form  action="" method="post">
-		    <div class="form-group">
+		   <div class="form-group">
                 <label for="project">Enter the project you worked on.</label>
-				<input type="text" class="form-control input-lg" name="project"
-                value="" placeholder="Project">
+                <select name="project" class="form-control input-lg">
+                    <option value="Adoption Event">Adoption Event</option>
+                    <option value="Clinic">Clinic</option>
+                    <option value="Fundraising Event">Fundraising Event</option>
+                    <option value="Grounds Maintainance">Grounds Maintenance</option>
+                    <option value="Office Work">Office Work</option>
+                    <option value="Socializtion">Socialization</option>
+                </select>
 			</div>
             <div class="form-group">
-                <label for="startdate">Enter the start date</label>
+                <label for="startdate">Enter the start date.</label>
 				<input type="date" class="form-control input-lg" name="startdate"><br>
-                <label for="starttime">Enter the start time</label>
+                <label for="starttime">Enter the start time.</label>
 				<input type="time" class="form-control input-lg" name="starttime">
 			</div>
             <div class="form-group">
-                <label for="enddate">Enter the end date</label>
+                <label for="enddate">Enter the end date.</label>
 				<input type="date" class="form-control input-lg" name="enddate"><br>
-                <label for="endtime">Enter the end time</label>
+                <label for="endtime">Enter the end time.</label>
 				<input type="time" class="form-control input-lg" name="endtime">
 			</div>
 
