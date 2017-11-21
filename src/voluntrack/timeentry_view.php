19572ac9249d735faf3e4d8fdca9ced4c8201cbc
Stan Sakl <stansakl@gmail.com>
2017-11-21 14:32:09 -0600

    Default project selection to Socialization

--- src/voluntrack/timeentry_view.php
+++ src/voluntrack/timeentry_view.php
@@ -24,7 +24,7 @@ require "htmlconstants.php";
                     <option value="Fundraising Event">Fundraising Event</option>
                     <option value="Grounds Maintainance">Grounds Maintenance</option>
                     <option value="Office Work">Office Work</option>
-                    <option value="Socializtion">Socialization</option>
+                    <option value="Socializtion" selected>Socialization</option>
                 </select>
 			</div>
             <div class="form-group">
