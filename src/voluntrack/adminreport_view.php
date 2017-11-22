7e0c831eedb87cba09203ce3aaf861d91c490816
Stan Sakl <stansakl@gmail.com>
2017-11-21 19:04:36 -0600

    Admin reporting stub.

---
+++ src/voluntrack/adminreport_view.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require "htmlconstants.php";
+//namespace voluntrack;
+//require '../vendor/autoload.php';
+?>
+
+ <!DOCTYPE html>
+ <?php echo HEADER; ?>
+
+
+ <body>
+ <?php require "navbar.php"; ?>
+	<div class="container">
+	  <div class="panel panel-primary">
+	  <div class="panel-heading">Organizational Time Report</div>
+	  <div class="panel-body">
+
+	  </div>
+	  </div>
+	  </div>
+	</div>
+ </body>
