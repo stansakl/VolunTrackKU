a0d08ad5c9cbadd28b209e9cd9520e19bb5ba154
Stan Sakl <stansakl@gmail.com>
2017-11-22 12:29:20 -0600

    Convert returned hours/minutes to decimal number of hours.

--- src/voluntrack/DBManager.php
+++ src/voluntrack/DBManager.php
@@ -155,7 +155,7 @@ class DBManager
                 u.NAME_LAST,
                 up.project_id,
                 p.Project_Name,
-                timediff(PROJECT_END_DATE_TIME, PROJECT_START_DATE_TIME) as hours
+                TIME_TO_SEC(TIMEDIFF(PROJECT_END_DATE_TIME, PROJECT_START_DATE_TIME))/3600 as hours
                 from user_project up
                 left outer join users u on u.user_id = up.user_id
                 left outer join project p on up.project_id = p.project_id
