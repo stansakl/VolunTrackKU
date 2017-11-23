a918660c4e04c41176c688b2b6f83f54bef8136f
Stan Sakl <stansakl@gmail.com>
2017-11-22 18:05:48 -0600

    Fix path to js file.

--- src/voluntrack/htmlconstants.php
+++ src/voluntrack/htmlconstants.php
@@ -5,5 +5,5 @@ define("HEADER", "<head>
     <link rel=\"stylesheet\" type=\"text/css\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">
     <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>
 	<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/voluntrack.css\">
-    <script src=\"js/voluntrack.js\"></script>
+    <script src=\"../js/voluntrack.js\"></script>
 </head>");
