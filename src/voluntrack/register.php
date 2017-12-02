f8c76612a1fa911e01a21edaafc574ec25a0cde9
Stan Sakl <stansakl@gmail.com>
2017-12-02 14:01:31 -0600

    Fix redirect bug

--- src/voluntrack/register.php
+++ src/voluntrack/register.php
@@ -7,16 +7,16 @@ $dbm = DBManager::get_instance();
 
 if ($_POST['password'] !== $_POST['confirmpwd']) {
     $_SESSION['error'] = "Passwords do not match!";
-    header("location: register.php");
+    header("location: register_view.php");
 }
 
 try {
     unset($_SESSION['error']);
     $dbm->register_user($_POST['firstname'],$_POST['lastname'],$_POST['middlename'],$_POST['email'],$_POST['password']);
-    header("location: register.php");
+    header("location: ../index.php");
 
 } catch (\Exception $e) {
     
     $_SESSION['error'] = "An error occurred registering the user. Please try again.";
-    header("location: register.php");
+    header("location: register_view.php");
 }
