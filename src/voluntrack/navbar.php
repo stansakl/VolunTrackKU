e558a2dd3c2a16a598c0af0952a47bb3054ad973
Stan Sakl <stansakl@gmail.com>
2018-02-09 21:40:56 -0600

    Fix typo

--- src/voluntrack/navbar.php
+++ src/voluntrack/navbar.php
@@ -15,7 +15,7 @@ if((isset($_SESSION['logged_in']) && ($_SESSION['logged_in'] == true))) {
   echo "<li><a href=\"voluntrack/timereport_view.php\"><span class=\"glyphicon glyphicon-list\"></span> My Report</a></li>";
   echo "</ul>";
   echo "<ul class=\"nav navbar-nav navbar-right custnav\">";
-  echo "<li class='userinfocust'>Logged in as : <span class=\"glyphicon glyphicon-user\"></span> "; echo $_SESSION['user']; echo "</li>";
+  echo "<li class='userinfocus'>Logged in as : <span class=\"glyphicon glyphicon-user\"></span> "; echo $_SESSION['user']; echo "</li>";
   echo "<li><a href=\"voluntrack/logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span> Log out</a></li>";  
   } else{
   echo "<ul class=\"nav navbar-nav custnav\">";
@@ -23,7 +23,7 @@ if((isset($_SESSION['logged_in']) && ($_SESSION['logged_in'] == true))) {
   echo "<li><a href=\"timereport_view.php\"><span class=\"glyphicon glyphicon-list\"></span> My Report</a></li>";
   echo "</ul>";
   echo "<ul class=\"nav navbar-nav navbar-right custnav\">";
-  echo "<li class='userinfocust'>Logged in as : <span class=\"glyphicon glyphicon-user\"></span> "; echo $_SESSION['user']; echo "</li>";
+  echo "<li class='userinfocus'>Logged in as : <span class=\"glyphicon glyphicon-user\"></span> "; echo $_SESSION['user']; echo "</li>";
   echo "<li><a href=\"logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span> Log out</a></li>";
   }
 }
